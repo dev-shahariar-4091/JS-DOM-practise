4e8577ba0637c5a44c690339eb8ecf3030ade8f5
Shahariarislam <shahariarislam6309@gmail.com>
2025-10-06 19:14:51 +0600

    Color is changed

--- first.js
+++ first.js
@@ -61,6 +61,7 @@ const paragraph = document.getElementById("myParagraph");
      const box = document.getElementById("box");
      box.addEventListener("mouseover", function() {
       box.style.backgroundColor = "red";
+      
     });
 
      box.addEventListener("mouseout", function() {
